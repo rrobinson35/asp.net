387d628e2bcbbbe25d11bdb6f2452af245846766
rrobinson35 <rhysrobinson321@hotmail.com>
2021-02-05 00:38:46 +0000

    Create details.cshtml.cs

---
+++ web-app/files/pages/details.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GroceryStore.Models;
+
+namespace GroceryStore.Pages
+{
+  public class DetailsModel : PageModel
+  {
+    
+    public List<GroceryItem> Foods = Inventory.ToList();
+    public GroceryItem CurrentFood;
+    
+  }
+  public async Task<IActionResult> OnGet(string name)
+    {
+      using (StreamWriter writer = new StreamWriter("log.txt", append: true))
+{
+  await writer.WriteLineAsync($"{DateTime.Now} {name}");
+}
+    CurrentFood = Foods.Find(food => food.Name.ToLower() == name.ToLower());
+
+    if(CurrentFood== null){
+
+      return "Not Found"
+    }
+    }
+}
